a0268e31a5ae46175a545fb950aa4e109510cd74
ashley <vothuykg123@gmail.com>
2023-02-17 17:25:36 +0700

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,93 @@
+const mongoose = require('mongoose')
+const validator = require('validator');
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Ten la bat buoc"],
+        maxLength: [50, "Ten phai duoi 50 ky tu"],
+    },
+    email: {
+        type: String,
+        required: [true, "Lam on nhap email"],
+        unique: true,
+        validate: [validator.isEmail, "Sai dinh dang email"],
+    },
+    password: {
+        type: String,
+        required: [true, "Mat khau la bat buoc"],
+        minlength: [8, "Mat khau co toi thieu 8 ky tu"],
+        select: false,
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    shop: {
+        shop: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Shop"
+        },
+        isCreated: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+});
+
+//Encrypting password before saving user
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+//Compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+//Return JWT Token
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_TIME,
+    });
+};
+
+//Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+    //generate token
+    const resetToken = crypto.randomBytes(20).toLocaleString("hex");
+    //hash and set to reset resetPasswordToken
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+    //token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
+};
+
+const myDB = mongoose.connection.useDb('tiki');
+const productInfo = myDB.model('User', userSchema);
+module.exports = productInfo;
