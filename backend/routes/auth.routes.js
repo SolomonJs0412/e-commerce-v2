f5c194afdcf74860a0f09adab65483b2a902173b
ashley <vothuykg123@gmail.com>
2023-02-19 16:33:16 +0700

    update route user and admin, add reset password, change password, send mail features

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const { isAuthenticatedUser, authorizeRoles } = require("../expressHelper/auth");
+const { isAuthenticatedUser } = require("../expressHelper/auth");
 
 const {
     createUser,
     loginUser,
     logoutUser,
-    getUserInfo } = require("../controllers/auth.controller");
+    getUserInfo,
+    updateUserInfo,
+    updatePassword,
+    resetPassword,
+    forgotPassword } = require("../controllers/auth.controller");
 
 router.route("/register").post(createUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(isAuthenticatedUser, logoutUser);
 router.route("/me").get(isAuthenticatedUser, getUserInfo);
+router.route("/me/info").put(isAuthenticatedUser, updateUserInfo);
+router.route("/me/info/password").put(isAuthenticatedUser, updatePassword);
+router.route("/password/reset/:token").put(resetPassword);
+router.route("/password/forgot").post(isAuthenticatedUser, forgotPassword);
 
 module.exports = router;
